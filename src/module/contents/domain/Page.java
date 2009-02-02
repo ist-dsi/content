3a79c326a179d27450d610dc560e4bcecde16d1b
Luis Cruz <luis.cruz@ist.utl.pt>
2009-02-02 19:31:42 +0000

    Added license.

--- src/module/contents/domain/Page.java
+++ src/module/contents/domain/Page.java
@@ -1,3 +1,27 @@
+/*
+ * @(#)Page.java
+ *
+ * Copyright 2009 Instituto Superior Tecnico, João Figueiredo, Luis Cruz, Paulo Abrantes, Susana Fernandes
+ * 
+ *      https://fenix-ashes.ist.utl.pt/
+ * 
+ *   This file is part of the Contents Module for the MyOrg web application.
+ *
+ *   The Contents Module is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.*
+ *
+ *   The Contents Module is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Contents Module. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 package module.contents.domain;
 
 import java.io.Serializable;
