f89b0027821e15f6b8646596d092a7098722909b
Luis Cruz <luis.cruz@ist.utl.pt>
2009-02-07 18:38:02 +0000

    Removed unused imports.

--- src/module/contents/domain/PageNode.java
+++ src/module/contents/domain/PageNode.java
@@ -28,8 +28,6 @@ package module.contents.domain;
 import myorg.domain.VirtualHost;
 import myorg.domain.contents.Node;
 import myorg.domain.groups.AnyoneGroup;
-import myorg.presentationTier.Context;
-import myorg.presentationTier.actions.ContextBaseAction;
 import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class PageNode extends PageNode_Base {
