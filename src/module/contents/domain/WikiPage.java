294ac6aedfff0a1adb8afe8951bd48e3fa9cebf4
Luis Cruz <luis.cruz@ist.utl.pt>
2009-08-21 15:06:14 +0000

    Start of experiment of wiki implementation.

---
+++ src/module/contents/domain/WikiPage.java
@@ -0,0 +1,91 @@
+package module.contents.domain;
+
+import java.io.Serializable;
+
+import module.contents.domain.Page.PageBean;
+import myorg.domain.VirtualHost;
+import myorg.domain.contents.INode;
+import myorg.domain.contents.Node;
+import pt.ist.fenixWebFramework.services.Service;
+import pt.ist.fenixWebFramework.util.DomainReference;
+import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
+
+public class WikiPage extends WikiPage_Base {
+
+    public static class WikiPageVersionBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	DomainReference<Node> parentNode;
+	DomainReference<VirtualHost> virtualHost;
+	MultiLanguageString title;
+	MultiLanguageString link;
+	MultiLanguageString content;
+
+	public WikiPageVersionBean() {
+	}
+
+	public WikiPageVersionBean(final VirtualHost virtualHost, final Node parentNode) {
+	    this();
+	    setParentNode(parentNode);
+	    setVirtualHost(virtualHost);
+	}
+
+	public MultiLanguageString getTitle() {
+	    return title;
+	}
+
+	public void setTitle(MultiLanguageString title) {
+	    this.title = title;
+	}
+
+	public MultiLanguageString getLink() {
+	    return link;
+	}
+
+	public void setLink(MultiLanguageString link) {
+	    this.link = link;
+	}
+
+	public MultiLanguageString getContent() {
+	    return content;
+	}
+
+	public void setContent(MultiLanguageString content) {
+	    this.content = content ;
+	}
+
+	public Node getParentNode() {
+	    return parentNode == null ? null : parentNode.getObject();
+	}
+
+	public void setParentNode(final Node parentNode) {
+	    this.parentNode = parentNode == null ? null : new DomainReference<Node>(parentNode);
+	}
+
+	public VirtualHost getVirtualHost() {
+	    return virtualHost == null ? null : virtualHost.getObject();
+	}
+
+	public void setVirtualHost(final VirtualHost virtualHost) {
+	    this.virtualHost = virtualHost == null ? null : new DomainReference<VirtualHost>(virtualHost);
+	}
+    }
+
+    public WikiPage() {
+        super();
+    }
+
+    public WikiPage(final WikiPageVersionBean wikiPageVersionBean) {
+	this();
+	new WikiPageVersion(this, wikiPageVersionBean.getTitle(), wikiPageVersionBean.getLink(), wikiPageVersionBean.getContent());
+	new WikiPageNode(wikiPageVersionBean.getVirtualHost(), wikiPageVersionBean.getParentNode(), this, null);
+    }
+
+    @Service
+    public static INode createNewPage(final WikiPageVersionBean wikiPageVersionBean) {
+	final WikiPage wikiPage = new WikiPage(wikiPageVersionBean);
+	return wikiPage.getWikiPageNode();
+    }
+
+}
---
+++ src/module/contents/domain/WikiPageNode.java
@@ -0,0 +1,37 @@
+package module.contents.domain;
+
+import myorg.domain.VirtualHost;
+import myorg.domain.contents.Node;
+import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
+
+public class WikiPageNode extends WikiPageNode_Base {
+    
+    public WikiPageNode() {
+        super();
+    }
+
+    public WikiPageNode(final VirtualHost virtualHost, final Node parentNode, final WikiPage wikiPage, final Integer order) {
+	this();
+	init(virtualHost, parentNode, order);
+	setWikiPage(wikiPage);
+    }
+
+    @Override
+    protected void appendUrlPrefix(StringBuilder stringBuilder) {
+	stringBuilder.append("/wiki.do?method=viewPage");	
+    }
+
+    @Override
+    public Object getElement() {
+	return getWikiPage();
+    }
+
+    @Override
+    public MultiLanguageString getLink() {
+	final WikiPage wikiPage = getWikiPage();
+	final WikiPageVersion wikiPageVersion = wikiPage.getLastWikiPageVersion();
+	return wikiPageVersion.getLink();
+
+    }
+    
+}
