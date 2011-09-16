23777b3eece65ccc0595ea26be14dec8909afb89
Nuno Diegues <nmld@ist.utl.pt>
2011-09-16 15:04:59 +0000

    New feature: Functionalities are provided as semantic links for entry points to nodes. These avoid the need for checksum and context in the url.

--- src/module/contents/domain/VersionedPageNode.java
+++ src/module/contents/domain/VersionedPageNode.java
@@ -6,7 +6,7 @@ import myorg.domain.groups.AnyoneGroup;
 import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class VersionedPageNode extends VersionedPageNode_Base {
-    
+
     public VersionedPageNode() {
 	super();
 	setAccessibilityGroup(AnyoneGroup.getInstance());
@@ -39,5 +39,10 @@ public class VersionedPageNode extends VersionedPageNode_Base {
     protected void appendUrlPrefix(final StringBuilder stringBuilder) {
 	stringBuilder.append("/pageVersioning.do?method=viewPage");
     }
-    
+
+    @Override
+    public boolean isAcceptsFunctionality() {
+	return false;
+    }
+
 }
